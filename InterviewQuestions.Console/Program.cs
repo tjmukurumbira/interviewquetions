9ce728bb5cff207300158c2052a008b83850b030
tjmukurumbira <tjmukurumbira@gmail.com>
2021-04-13 20:40:18 +0200

    Added Console

---
+++ InterviewQuestions.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using InterviewQuestions.Lib;
+using System;
+
+namespace InterviewQuestions.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz();
+
+        }
+
+        private static void FizzBuzz()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                System.Console.WriteLine($"{i} {FizzBuzzer.GetMessage(i)}");
+            }
+        }
+    }
+}
